fdc98b728a4d378142491091c313027e0a45a9b1
Tyler Bowman <tylerbowman@Tylers-MacBook-Pro.local>
2018-03-05 14:07:03 -0600

    hey steve

--- app/app.js
+++ app/app.js
@@ -1,2 +1,28 @@
 let app = angular.module("TeacherHub", ["ngRoute"])
 
+
+const isAuth = AuthFactory => new Promise ((resolve, reject) => {
+    if (AuthFactory.isAuthenticated()){
+        console.log("User is authenticated, resolve route promise")
+        resolve()
+    } else {
+        console.log("User is not authenticated, reject route promise")
+        reject()
+    }
+})
+
+angular.module("TeacherHub").config(function ($routeProvider){
+    $routeProvider
+        .when("/teachers/teacherDash", {
+            templateUrl: "../teachers/partials/teacherDash.html",
+            controller: "teacherDashController",
+            resolve: {isAuth}
+        })
+        .when("/classes/classList", {
+            templateUrl: "../classes/partials/classList.html",
+            controller: "classListCtrl",
+            resolve: {isAuth}
+        })
+        .when("/")
+})
+
