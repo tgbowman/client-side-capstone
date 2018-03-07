47948800032ce4e9f53dc6945fcff00e8a60449f
Tyler Bowman <tylerbowman@Tylers-MacBook-Pro-2.local>
2018-03-07 10:07:54 -0600

    updated angular routes on app.js

--- app/app.js
+++ app/app.js
@@ -13,16 +13,74 @@ const isAuth = AuthFactory => new Promise ((resolve, reject) => {
 
 angular.module("TeacherHub").config(function ($routeProvider){
     $routeProvider
+        .when("/auth", {
+            templateUrl:"../app/auth/partials/login.html",
+            controller: "AuthCtrl"
+        })
+        .when("/auth/register", {
+            templateUrl: "../app/auth/partials/register.html",
+            controller: "AuthCtrl"
+        })
         .when("/teachers/teacherDash", {
-            templateUrl: "../teachers/partials/teacherDash.html",
-            controller: "teacherDashController",
+            templateUrl: "../app/teachers/partials/teacherDash.html",
+            controller: "teacherDashCtrl",
             resolve: {isAuth}
         })
         .when("/classes/classList", {
-            templateUrl: "../classes/partials/classList.html",
+            templateUrl: "../app/classes/partials/classList.html",
             controller: "classListCtrl",
             resolve: {isAuth}
         })
-        .when("/")
+        .when("/assignments/assignmentDash/:assignmentId", {
+            templateUrl:"../app/assignments/partials/assignmentDash.html",
+            controller: "assignmentDashCtrl",
+            resolve: {isAuth}
+        })
+        .when("/assignments/assignmentCreator", {
+            templateUrl:"../app/assignments/partials/assignmentCreator.html",
+            controller: "assignmentCreateCtrl",
+            resolve: {isAuth}
+        })
+        .when("/classes/classCreator", {
+            templateUrl:"../app/classes/partials/classCreator.html",
+            controller:"classCreatorCtrl",
+            resolve: {isAuth}
+        })
+   
+        .when("/classes/classDash/:classId", {
+            templateUrl:"../app/classes/partials/classDash.html",
+            controller:"classDashCtrl",
+            resolve: {isAuth}
+        })
+        .when("/students/addStudent",{
+            templateUrl:"../app/students/partials/addStudent.html",
+            controller:"addStudentCtrl",
+            resolve: {isAuth}
+        })
+        .when("/students/studentList", {
+            templateUrl: "../app/students/partials/studentList.html",
+            controller:"studentListCtrl",
+            resolve: {isAuth}
+        })
+        .when("/students/studentDash/:studentId", {
+            templateUrl:"../app/students/partials/studentDash.html",
+            controller:"studentDashCtrl",
+            resolve: {isAuth}
+        })
+        .when("/classes/classStudentDash/:classId", {
+            templateUrl:"../app/classes/partials/classStudentDash.html",
+            controller:"classStudentDashCtrl",
+            resolve:{isAuth}
+        })
+        .when("/discipline/disciplineForm", {
+            templateUrl:"../app/discipline/partials/disciplineForm.html",
+            controller:"disciplineFormCtrl",
+            resolve: {isAuth}
+        })
+        .when("/discipline/disciplineDash/:incidentId", {
+            templateUrl:"../app/discipline/partials/disciplineDash.html",
+            controller:"disciplineDashCtrl",
+            resolve:{isAuth}
+        })
 })
 
--- app/assignments/controllers/assignmentDashCtrl.js
+++ app/assignments/controllers/assignmentDashCtrl.js
@@ -1,32 +1,32 @@
-angular.module("TeacherHub").controller("assignmentDashCtrl", function($scope, assignmentFactory, assignmentClassFactory, studentFactory, classStudentFactory, classFactory, gradeFactory, $routeParams, $location){
+angular.module("TeacherHub").controller("assignmentDashCtrl", function ($scope, assignmentFactory, assignmentClassFactory, studentFactory, classStudentFactory, classFactory, gradeFactory, $routeParams, $location) {
     $scope.students = []
     $scope.currentClass = classFactory.currentClass
     $scope.currentAssignment = null
     $scope.assignmentId = $routeParams.assignmentId
     $scope.myAssignment = null
 
-    $scope.currentAssignment = assignmentFactory.assignmentCache.filter(ass=> {
+    $scope.currentAssignment = assignmentFactory.assignmentCache.filter(ass => {
         return $scope.assignmentId === ass.id
     })[0]
     console.log("The current assignment is:", $scope.currentAssignment)
     assignmentFactory.currentAssignment = $scope.currentAssignment
     assignmentFactory.editMode.assId = $scope.currentAssignment.id
 
-    $scope.getStudents = function() {
+    $scope.getStudents = function () {
         console.log($scope.currentClass.id)
-        $scope.myAssignment = assignmentClassFactory.assignmentRelCache.filter(rel=>{return rel.classId === $scope.currentClass.id && rel.assignmentId === $scope.assignmentId })
-        $scope.assignmentName = assignmentFactory.assignmentCache.filter(assignmentObj => {return assignmentObj.id === $scope.myAssignment[0].assignmentId})[0].title
+        $scope.myAssignment = assignmentClassFactory.assignmentRelCache.filter(rel => { return rel.classId === $scope.currentClass.id && rel.assignmentId === $scope.assignmentId })
+        $scope.assignmentName = assignmentFactory.assignmentCache.filter(assignmentObj => { return assignmentObj.id === $scope.myAssignment[0].assignmentId })[0].title
         console.log($scope.myAssignment)
-        
-        let gradeArray = gradeFactory.gradeCache.filter(grade => {return grade.assignmentClassId === $scope.myAssignment[0].id})
+
+        let gradeArray = gradeFactory.gradeCache.filter(grade => { return grade.assignmentClassId === $scope.myAssignment[0].id })
         console.log(gradeArray)
-        gradeArray.forEach(gradeObject=>{
+        gradeArray.forEach(gradeObject => {
             console.log(gradeObject)
             console.log(studentFactory.cachedStudents)
-            let studentObj = studentFactory.cachedStudents.filter(student=>{
+            let studentObj = studentFactory.cachedStudents.filter(student => {
                 return student.id === gradeObject.studentId
             })[0]
-            if(studentObj){
+            if (studentObj) {
                 console.log(studentObj)
                 let newStudent = {
                     "firstName": studentObj.studentFirstName,
@@ -37,37 +37,38 @@ angular.module("TeacherHub").controller("assignmentDashCtrl", function($scope, a
                     "gradeId": gradeObject.id
                 }
                 console.log(studentObj)
-                $scope.students.push(newStudent)}
+                $scope.students.push(newStudent)
+            }
         })
         console.log($scope.students)
     }
 
-    $scope.updateGrade = function(studentObj){
-        
-        let gradeObj = gradeFactory.gradeCache.filter(grade=>{return studentObj.studentId === grade.studentId && $scope.myAssignment[0].id === grade.assignmentClassId})
+    $scope.updateGrade = function (studentObj) {
+
+        let gradeObj = gradeFactory.gradeCache.filter(grade => { return studentObj.studentId === grade.studentId && $scope.myAssignment[0].id === grade.assignmentClassId })
         gradeObj[0].grade = studentObj.grade
         console.log(gradeObj)
-        
-        
+
+
         gradeFactory.update(gradeObj[0])
-            .then(r=>{
+            .then(r => {
                 console.log("grade updated successfully")
             })
     }
 
-    $scope.toEdit = function(){
+    $scope.toEdit = function () {
         assignmentFactory.editMode.enabled = true
         $location.url("/assignments/assignmentCreator")
     }
-    
-    
 
-    $scope.backToClass = function() {
+
+
+    $scope.backToClass = function () {
         $location.url(`/classes/classDash/${$scope.currentClass.id}`)
     }
 
     $scope.getStudents()
-    
+
 
 
 })
